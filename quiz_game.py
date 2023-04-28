220f935325a8ad4e15a7d752405f7bd296ca98c1
webdevsimone <114842090+webdevsimone@users.noreply.github.com>
2023-04-28 00:33:50 -0400

    added score

--- quiz_game.py
+++ quiz_game.py
@@ -1,41 +1,49 @@
 print("Welcome to my Quiz")
 
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play? ").lower()
 
-if playing.lower() != "yes" or "Yes":
+if playing != "yes":
     quit()
 
 print("Cool! Let's Play :) ")
 
+score = 0
+
 answer = input("How many Zodiac signs are there in Western Astrology? ")
-if answer.lower() == "12" or "twelve":
+if answer == "12" or "twelve" or "Twelve":
     print("YAY! Correct!")
+    score += 1
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign represents the lion? ")
 if answer.lower() == "Leo" or "leo":
     print("YAY! Correct!")
+    score += 1
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What element is the Zodaic sign Aries know for? ")
 if answer.lower() == "Fire" or "fire":
     print("YAY! Correct!")
+    score += 1
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign is known as 'The Virgin'? ")
 if answer.lower() == "Virgo" or "virgo":
     print("YAY! Correct!")
+    score += 1
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign is a Earth Sign? (please input only one answer) ")
-if answer.lower() == "Capricorn" or "capricorn" or "Taurus" or "taurus" or "virgo" or "Virgo" or "Sagittarius" or "sagittarius":
+if answer == "Capricorn" or "capricorn" or "Taurus" or "taurus" or "virgo" or "Virgo" or "Sagittarius" or "sagittarius":
     print("YAY! Correct!")
+    score += 1
 else: 
     print("Sorry, Incorrect!")
 
-
+print("Hey! You got " + str(score) + " questions correct! Thanks for playing! ")
+print("You got " + str((score/5) * 100) + "%!")
