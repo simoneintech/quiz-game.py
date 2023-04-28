037cf3c690a2e762ade93a3f43a943f0f551db15
webdevsimone <114842090+webdevsimone@users.noreply.github.com>
2023-04-27 23:45:03 -0400

    added .lower so user input can automatically go to all lowercase in order to avoid error

--- quiz_game.py
+++ quiz_game.py
@@ -3,37 +3,37 @@ print("Welcome to my Quiz")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes" or "Yes":
     quit()
 
 print("Cool! Let's Play :) ")
 
 answer = input("How many Zodiac signs are there in Western Astrology? ")
-if answer == "12" or "twelve":
+if answer.lower() == "12" or "twelve":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign represents the lion? ")
-if answer == "Leo" or "leo":
+if answer.lower() == "Leo" or "leo":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What element is the Zodaic sign Aries know for? ")
-if answer == "Fire" or "fire":
+if answer.lower() == "Fire" or "fire":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign is known as 'The Virgin'? ")
-if answer == "Virgo" or "virgo":
+if answer.lower() == "Virgo" or "virgo":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
 
 answer = input("What Zodiac sign is a Earth Sign? (please input only one answer) ")
-if answer == "Capricorn" or "capricorn" or "Taurus" or "taurus" or "virgo" or "Virgo" or "Sagittarius" or "sagittarius":
+if answer.lower() == "Capricorn" or "capricorn" or "Taurus" or "taurus" or "virgo" or "Virgo" or "Sagittarius" or "sagittarius":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
