77818dc4244bd978a474c66ab0fb9ab1818b023d
webdevsimone <114842090+webdevsimone@users.noreply.github.com>
2023-04-27 23:01:41 -0400

    added input, if statement and first quiz question

---
+++ quiz_game.py
@@ -0,0 +1,11 @@
+print("Welcome to my Quiz")
+
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Cool! Let's Play :) ")
+
+answer = input("How many Zodiac signs are there in Western Astrology?")
