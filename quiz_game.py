4b640553aaf3ec61a6a86b226437515e3c32b81c
webdevsimone <114842090+webdevsimone@users.noreply.github.com>
2023-04-27 23:30:19 -0400

    fixed indentation error, causing output to print the answer correct instead of starting over to the next question

--- quiz_game.py
+++ quiz_game.py
@@ -13,3 +13,29 @@ if answer == "12" or "twelve":
     print("YAY! Correct!")
 else: 
     print("Sorry, Incorrect!")
+
+answer = input("What Zodiac sign represents the lion? ")
+if answer == "Leo" or "leo":
+    print("YAY! Correct!")
+else: 
+    print("Sorry, Incorrect!")
+
+answer = input("What element is the Zodaic sign Aries know for? ")
+if answer == "Fire" or "fire":
+    print("YAY! Correct!")
+else: 
+    print("Sorry, Incorrect!")
+
+answer = input("What Zodiac sign is known as 'The Virgin'? ")
+if answer == "Virgo" or "virgo":
+    print("YAY! Correct!")
+else: 
+    print("Sorry, Incorrect!")
+
+answer = input("What Zodiac sign is a Earth Sign? (please input only one answer) ")
+if answer == "Capricorn" or "capricorn" or "Taurus" or "taurus" or "virgo" or "Virgo" or "Sagittarius" or "sagittarius":
+    print("YAY! Correct!")
+else: 
+    print("Sorry, Incorrect!")
+
+
